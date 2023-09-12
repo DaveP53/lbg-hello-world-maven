7b84436824d268b3de1ea1e000c7963d84191f5c
Dave Pitchers <dave.pitchers@hotmail.com>
2023-09-12 14:29:33 +0100

    added names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
+        // comments here dave was here
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Dave"));
+        System.out.println(sayHelloToSomeone("Helen"));
+        System.out.println(sayHelloToSomeone("Graham"));
+        System.out.println(sayHelloToSomeone("Alicia"));
         System.out.println(sayGoodbye());
     }
 
